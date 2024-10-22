7b691fbb996100c83b3d704e62f59e5278b3022c
Himanshu Pandey <himanshu.it09@gmail.com>
2024-10-22 23:50:53 +0530

    Updating Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,15 @@
 const Hero = () => {
     return (
-      <div className="my-10 mx-36 bg-custom h-150">
+      <div className="my-10 mx-36 bg-custom h-150 flex justify-center">
+        <div className="w-1/3">
+
+        </div>
+        <div className="w-2/3 flex ">
+        <h1 className="text-5xl mt-14 w-96 leading-normal">WAY KAMBAS
+        MINI EBONY</h1>
+
+        <div class="h-4 bg-black w-1/4"></div>
+        </div>
         
       </div>
     );

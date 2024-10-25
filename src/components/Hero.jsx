4078919fe68ced5399f558a674f8fdb61ed5628f
Himanshu Pandey <himanshu.it09@gmail.com>
2024-10-25 23:31:30 +0530

    Updating the hero section adding sliders

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,29 @@
 import watch from '../assets/Hero_Watch.svg';
 import watch2 from '../assets/Hero_Watch.png';
+import line from '../assets/line.png';
+import left from '../assets/left.png'
+import right from '../assets/right.png'
 import line1 from '../assets/line1.png';
 import line2 from '../assets/line2.png';
 import line3 from '../assets/line3.png';
 
 const Hero = () => {
   return (
-    <div className="my-9 mx-36 bg-custom h-150 flex justify-center relative">
+    
+    <div >
       {/* Image and text overlay */}
-      <div className="absolute inset-0 flex flex-col justify-center items-center z-10 ">
-        <img src={line1} alt="line1" className="w-150 h-auto  mt-24 ml-auto" />
-        <img src={line2} alt="line2" className="w-150 h-auto  mt-24 ml-auto" />
-        <img src={line3} alt="line3" className="w-150 h-auto  mt-24 ml-auto" />
-      </div>
 
+      <div className="absolute inset-0 flex flex-col justify-center items-center z-10 ">
+        <img src={line} alt="line2" className="w-150 h-auto  mb-40 ml-auto" />
+        {/* <img src={line1} alt="line1" className="relative w-150 h-auto mt-4  ml-auto " />
+        <img src={line2} alt="line2" className="w-150 h-auto  mt-0 ml-auto" />
+        <img src={line3} alt="line3" className="w-150 h-auto  mt-0 ml-auto" /> */}
+      </div>
+      
+      <div className="my-9 mx-36 bg-custom h-150 flex justify-center relative">
       {/* Watch image */}
       <div className="w-1/3 relative z-0">
-        <img src={watch2} alt="Watch IMG" className='w-100 h-100 mt-16' />
+        <img src={watch2} alt="Watch IMG" className='w-200 h-auto mt-16' />
         {/* Optional: <img src={watch} alt="Watch IMG" className='w-96 h-100 mt-32 ' /> */}
       </div>
 
@@ -33,13 +40,28 @@ const Hero = () => {
           <div className="bg-red-500 flex justify-center items-center mt-6 h-14 w-48">
             <h1 className="text-neutral-50">Add to cart</h1>
           </div>
-          <div className="border border-red-500 flex justify-center items-center mt-6 h-14 w-48">
+          <div className="border border-red-500 flex justify-center items-center mt-6 h-14 w-36">
             <h1 className="text-red-400">3 LEFT</h1>
           </div>
         </div>
+        <div className="flex gap-3 ml-100 mb-24 z-20 relative  ">
+          <div className="border border-red-500 flex justify-center items-center mt-3  h-12 w-12">
+            <img src={left} alt="" />
+          </div>
+          <div className=" bg-red-500 flex justify-center items-center mt-3 h-12 w-12">
+            <img src={right} alt="" />
+          </div>
+        </div>
       </div>
+
+      
+
+      </div>
+
+     
     </div>
   );
 };
 
 export default Hero;
+
